92cfab66a220a9085f13b1d67f7db47f150d3fae
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2024-11-13 20:48:42 -0500

    Update sync-3k-Repo-w-sudo.sh

--- sync-3k-Repo-w-sudo.sh
+++ sync-3k-Repo-w-sudo.sh
@@ -1,1 +1,14 @@
-ok
+#!/bin/bash
+
+cd MULTI-USER-DUNGEONS/
+
+#i had to use abosulte path since I was change directories ->> since it was getting file not found
+
+git pull origin main
+
+cd /home/ubuntu/
+
+cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/cbot.tin cbot.tin
+cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/color.tin color.tin
+cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/main.tin main.tin
+cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/leetclass.tin leetclass.tin
