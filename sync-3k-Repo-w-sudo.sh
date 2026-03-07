b424ed7cbc0da4930e88cb9374604596cb5b1530
I-dream-in-cod3 <147734328+I-dream-in-cod3@users.noreply.github.com>
2026-03-07 15:08:57 -0500

    Change directory and update file copy paths
    Updated the script to change the working directory to /home/git3k and copy .tin files from the new location.

--- sync-3k-Repo-w-sudo.sh
+++ sync-3k-Repo-w-sudo.sh
@@ -8,14 +8,16 @@ cd MULTI-USER-DUNGEONS/
 #update repo
 git pull origin main
 
-#go to home directory
-cd /home/ubuntu/
-
+#go to local directory
 #if you don't specify specific folder cd goes to the default directory for root user ie /root not /home/user or /home/patrick/ etc
+#pretty sure this whole script can only run with sudo or root permissions 
 
-#copy .tin files to home directory
-cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/cbot.tin cbot.tin
-cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/color.tin color.tin
-cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/main.tin main.tin
-cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/leetclass.tin leetclass.tin
-cp /home/ubuntu/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/hotelmorph.tin hotelmorph.tin
+cd /home/git3k/
+
+
+#copy .tin files to local directory
+cp /home/git3k/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/cbot.tin cbot.tin
+cp /home/git3k/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/color.tin color.tin
+cp /home/git3k/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/main.tin main.tin
+cp /home/git3k/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/leetclass.tin leetclass.tin
+cp /home/git3k/MULTI-USER-DUNGEONS/3K/iOS-PROTOTYPE/hotelmorph.tin hotelmorph.tin
